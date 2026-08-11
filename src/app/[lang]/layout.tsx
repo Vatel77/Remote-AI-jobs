5a07321f7c04a87067c0f85c77c6256144ac9796
Vatel77 <vatel06400@gmail.com>
2026-08-11 09:31:01 +0200

    Update layout.tsx

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
-import "../../globals.css";
+import "../globals.css";
 import Link from "next/link";
 import { dictionaries, Locale } from "../../i18n/dictionaries";
 import LanguageSwitcher from "@/components/LanguageSwitcher";
